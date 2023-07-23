895b0124741e8b16a973a6131acc30b8354b85e0
lubahabarta <lubahabarta@gmail.com>
2023-07-23 19:45:01 +0200

    added grainy overlay

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,10 @@
 <template>
+    <svg class="fixed top-0 w-full h-full z-[999] pointer-events-none opacity-50 mix-blend-difference">
+        <filter id="noise-filter">
+            <feTurbulence type="fractalNoise" baseFrequency="1.3" />
+        </filter>
+        <rect width="100%" height="100%" filter="url(#noise-filter)" />
+    </svg>
     <Navigation />
     <slot />
     <Footer />
