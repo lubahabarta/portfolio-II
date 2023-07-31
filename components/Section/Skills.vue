1ad274a4a1362fcff61d59f3723250622cbe9a59
lubahabarta <lubahabarta@gmail.com>
2023-07-31 19:27:45 +0200

    added images

--- components/Section/Skills.vue
+++ components/Section/Skills.vue
@@ -1,3 +1,44 @@
+<script setup>
+const slider = {
+    first: [
+        { src: '/img/logos/html.png', alt:"html" },
+        { src: '/img/logos/css.png', alt:"css" },
+        { src: '/img/logos/javascript.png', alt:"javascript" },
+        { src: '/img/logos/typescript.png', alt:"typescript" },
+        { src: '/img/logos/node.png', alt:"node" },
+        { src: '/img/logos/react.png', alt:"react" },
+        { src: '/img/logos/redux.png', alt:"redux" },
+        { src: '/img/logos/vite.png', alt:"vite" },
+        { src: '/img/logos/vue.png', alt:"vue" },
+        { src: '/img/logos/svelte.png', alt:"svelte" },
+    ],
+    second: [
+        { src: '/img/logos/sass.png', alt:"sass" },
+        { src: '/img/logos/tailwind.png', alt:"tailwind" },
+        { src: '/img/logos/nuxt.png', alt:"nuxt" },
+        { src: '/img/logos/nette.png', alt:"nette" },
+        { src: '/img/logos/php.png', alt:"php" },
+        { src: '/img/logos/firebase.png', alt:"firebase" },
+        { src: '/img/logos/mysql.png', alt:"mysql" },
+        { src: '/img/logos/git.png', alt:"git" },
+        { src: '/img/logos/github.png', alt:"github" },
+        { src: '/img/logos/bitbucket.png', alt:"bitbucket" },
+    ],
+    third: [
+        { src: '/img/logos/cs.png', alt:"cs" },
+        { src: '/img/logos/photoshop.png', alt:"photoshop" },
+        { src: '/img/logos/illustrator.png', alt:"illustrator" },
+        { src: '/img/logos/vue.png', alt:"vue" },
+        { src: '/img/logos/nuxt.png', alt:"nuxt" },
+        { src: '/img/logos/svelte.png', alt:"svelte" },
+        { src: '/img/logos/tailwind.png', alt:"tailwind" },
+        { src: '/img/logos/react.png', alt:"react" },
+        { src: '/img/logos/javascript.png', alt:"javascript" },
+        { src: '/img/logos/typescript.png', alt:"typescript" },
+    ],
+};
+</script>
+
 <template>
     <section id="skills" class="w-full h-[100vh] my-[10vh] flex justify-center">
         <div class="w-[80vw] flex flex-col justify-around">
@@ -9,85 +50,22 @@
                 after:h-full after:w-10 after:absolute after:right-0 after:top-0 after:bg-gradient-to-l after:from-white after:to-transparent after:z-10">
                 <div class="w-full overflow-hidden">
                     <div class="flex w-[calc((14vh+1rem)*20)] sm:w-[calc((28vh)*20)] animate-slider">
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
+                        <Skill v-for="item in slider.first" :src="item.src" :alt="item.alt" />
+                        <Skill v-for="item in slider.first" :src="item.src" :alt="item.alt" />
                     </div>
                 </div>
 
                 <div class="w-full overflow-hidden mt-4">
                     <div class="flex w-[calc((14vh+1rem)*20)] sm:w-[calc((28vh)*20)] animate-reverseSlider">
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
+                        <Skill v-for="item in slider.second" :src="item.src" :alt="item.alt" />
+                        <Skill v-for="item in slider.second" :src="item.src" :alt="item.alt" />
                     </div>
                 </div>
 
                 <div class="w-full overflow-hidden mt-4">
                     <div class="flex w-[calc((14vh+1rem)*20)] sm:w-[calc((28vh)*20)] animate-slider">
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
-                        <Skill src="https://miro.medium.com/v2/resize:fit:300/1*R4c8lHBHuH5qyqOtZb3h-w.png" alt="firebase-logo" />
+                        <Skill v-for="item in slider.third" :src="item.src" :alt="item.alt" />
+                        <Skill v-for="item in slider.third" :src="item.src" :alt="item.alt" />
                     </div>
                 </div>
             </div>
--- components/Skill.vue
+++ components/Skill.vue
@@ -12,7 +12,7 @@ const { src } = defineProps({
 </script>
 
 <template>
-    <div class="flex h-[14vh] aspect-square justify-center items-center rounded-lg border-[1px] border-gray-400 mr-4 sm:mr-[14vh]">
-        <img :src="src" :alt="alt" class="w-2/5 h-2/5" />
+    <div class="flex h-[14vh] aspect-square justify-center items-center bg-white shadow-lg rounded-[10%] border-[1px] border-gray-400 mr-4 sm:mr-[14vh]">
+        <img :src="src" :alt="alt" class="w-[45%] h-[45%]" />
     </div>
 </template>
