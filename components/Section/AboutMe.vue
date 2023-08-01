bc361273d367d4bc3725f6486f3251925c711273
lubahabarta <lubahabarta@gmail.com>
2023-08-01 18:24:06 +0200

    small changes, for push

--- components/Section/AboutMe.vue
+++ components/Section/AboutMe.vue
@@ -2,7 +2,9 @@
     <section id="about-me" class="w-full flex justify-center">
         <div class="w-[80vw]">
             <div class="w-full grid gap-[1px] grid-cols-3 bg-[radial-gradient(#9ca3af,rgba(0,0,0,0)60%)] bg-fixed">
-                <div class="bg-white col-span-1 flex items-center justify-center">some text</div>
+                <div class="bg-white col-span-1 flex items-center justify-center p-[10%]">
+                    <img src="/img/me.jpg" alt="me.jpg" class="w-full h-full object-fit" />
+                </div>
                 <div class="col-span-1 grid gap-[1px] grid-cols-3">
                     <div class="bg-white col-span-1 aspect-square"></div>
                     <div class="bg-white col-span-1 aspect-square"></div>
